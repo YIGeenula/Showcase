2332c1ebb6a6f7e556270547e1573dbd0320f7b3
YIGeenula <YIGeenula>
2026-02-28 21:25:07 +0530

    add graphics design section (HIDE)

--- app/page.js
+++ app/page.js
@@ -9,6 +9,7 @@ import Navigation from '@/components/Navigation';
 import Hero from '@/components/Hero';
 import About from '@/components/About';
 import Skills from '@/components/Skills';
+//import GraphicDesign from '@/components/GraphicDesign';
 import Projects from '@/components/Projects';
 import Contact from '@/components/Contact';
 import Footer from '@/components/Footer';
@@ -46,7 +47,9 @@ export default function Home() {
           <Hero />
           <About />
           <Skills />
+
           <Projects />
+          {/* <GraphicDesign /> */}
           <Contact />
 
           <Footer />
